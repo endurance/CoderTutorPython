752e1b731a7523d59dd7714aee6991df2dbb3362
Endurance Idehen <jeidehen@gmail.com>
2017-03-12 22:32:51 -0500

    Corrected a bunch of annoying things you did

--- project1/get_used_to_python.py
+++ project1/get_used_to_python.py
@@ -1,30 +1,26 @@
 
 # ____Variables
 
-#age = 24
-#Make a couple variables:
-#a bool variable
-#a float variable
-#a string variable
-#print out all the types of those variables
-#and finally, print out all of the values of those variables to the string to the SCREEN
+# Make a couple variables:
+# a bool variable
+# a float variable
+# a string variable
+# print out all the types of those variables
+# and finally, print out all of the values of those variables to the string to the SCREEN
 
-#a bool variable
+# a bool variable
 all_apples_are_red = True
-#type(all_apples_are_red)
 nope = type(all_apples_are_red)
 print(nope)
 print(all_apples_are_red)
 
-#a float variable
-milage = 1000.0
-type(milage)
-car_info = type(milage)
+# a float variable
+mileage = 1000.0
+car_info = type(mileage)
 print(car_info)
 
-#a string variable
+# a string variable
 cat_name = "ZeldaPrincess"
-type(cat_name)
 how_to_call_my_cat = type(cat_name)
 print(how_to_call_my_cat)
 
