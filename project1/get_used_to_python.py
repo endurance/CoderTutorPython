31f026b989706877b25d781a7a57daef1e35906b
Katie Prather <programmingprather@gmail.com>
2017-03-12 22:21:08 -0500

    finished the variables and started the functions - python

---
+++ project1/get_used_to_python.py
@@ -0,0 +1,35 @@
+
+# ____Variables
+
+#age = 24
+#Make a couple variables:
+#a bool variable
+#a float variable
+#a string variable
+#print out all the types of those variables
+#and finally, print out all of the values of those variables to the string to the SCREEN
+
+#a bool variable
+all_apples_are_red = True
+#type(all_apples_are_red)
+nope = type(all_apples_are_red)
+print(nope)
+print(all_apples_are_red)
+
+#a float variable
+milage = 1000.0
+type(milage)
+car_info = type(milage)
+print(car_info)
+
+#a string variable
+cat_name = "ZeldaPrincess"
+type(cat_name)
+how_to_call_my_cat = type(cat_name)
+print(how_to_call_my_cat)
+
+#____Functions
+def myfunction(str):
+    "i_chose_to_use_an_optional_statement"
+    print(str)
+    return
